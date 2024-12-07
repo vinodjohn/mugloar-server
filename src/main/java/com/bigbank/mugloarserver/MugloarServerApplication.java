8fa7b46d2bbce6b665cd21d7c33d045ab32bc13d
vinodjohn <vinodjohn56@gmail.com>
2024-12-07 05:33:51 +0200

    Initial commit

---
+++ src/main/java/com/bigbank/mugloarserver/MugloarServerApplication.java
@@ -0,0 +1,13 @@
+package com.bigbank.mugloarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugloarServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MugloarServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigbank/mugloarserver/MugloarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbank.mugloarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MugloarServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
